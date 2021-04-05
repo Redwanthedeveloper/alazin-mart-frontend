d1a7b36738847d09bd3023727ef8870ce3cd8f5b
Redwanthedeveloper <redwan.thedeveloper@gmail.com>
2021-04-05 17:46:25 +0600

    bug fixed

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { Button, Card, Col, Row } from 'react-bootstrap';
 
 const Product = ({ product, buyHandler }) => {
-  console.log(product);
   return (
     <Card className='my-3 p-3 rounded'>
       <Link to={`/product/${product._id}`}>
